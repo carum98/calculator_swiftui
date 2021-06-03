5b81631ed9cc874a9c8cbd536a19c823b809841f
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-06-02 21:58:03 -0600

    Buttons structure

--- Lab2/ContentView.swift
+++ Lab2/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    let buttons = [
+        ["7", "8", "9", "%"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ForEach(buttons, id: \.self) {row in
+                HStack{
+                    ForEach(row, id: \.self) { value in
+                        CardView(content: value)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CardView: View {
+    var content: String
+    
+    var body: some View {
+        Text(content).accentColor(.white)
+            .font(.system(size: 32))
+            .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.white)
+            .background(Color.yellow)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro Max")
     }
 }
