c0aabd80b09900de28198c150860670160d263cb
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-06-02 20:30:18 -0600

    Initial Commit

---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
